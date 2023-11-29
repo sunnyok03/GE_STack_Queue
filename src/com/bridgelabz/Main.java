d015051bc8f1b6adbdf3c16c37e168c69ce3c4e5
sunnyok03 <sunnyok03@gmail.com>
2023-11-29 23:10:44 +0530

    UC1 push method in stack

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Ability to create a Stack of 56->30->70
+        Stack<Integer> UC1 = new Stack<>();
+        UC1.push(70);
+        UC1.push(30);
+        UC1.push(56);
 
 
     }
--- src/com/bridgelabz/Stack.java
+++ src/com/bridgelabz/Stack.java
@@ -9,4 +9,17 @@ public class Stack<T> {
     private Node<T> head;
     private int size;
 
+    // insert a new element in the stack
+    public void push(T data){
+        Node<T> newNode = new Node<>(data);
+        size++;
+        if(head == null){
+            head = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
 }
